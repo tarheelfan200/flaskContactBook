e132272ef9dbce21bcd166b233773920ff1ab4ae
tarheelfan200 <tarheelfan3951@gmail.com>
2024-11-12 08:10:53 -0500

    update

--- app.py
+++ app.py
@@ -4,21 +4,24 @@ from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__)
 
+contacts = []
+
 @app.route("/")
 def home():
     return render_template("home.html")
 
-@app.route("/add_contact.html")
+@app.route("/add_contact")
 def add_contact():
     return render_template("add_contact.html")
 
 @app.route("/handleAddContact", methods=["POST"])
 def handleAddContact():
 
-    name = request.form["contact-name"]
-    number = request.form["contact-number"]
+    name = request.form.get("contact-name")
+    number = request.form.get("contact-number")
+    contacts.append([name,number])
 
-    return f"Contact Name:{name}\t Contact Number:{number}"
+    return render_template("/show_contacts.html", x = name, y = number)
 
 if __name__ == "__main__":
     app.run(debug=True, port=80, host="0.0.0.0")
