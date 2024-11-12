0e801aecf71cd06955c591efad5ef51165bef39d
tarheelfan200 <tarheelfan3951@gmail.com>
2024-11-12 08:11:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -15,13 +15,13 @@ def add_contact():
     return render_template("add_contact.html")
 
 @app.route("/handleAddContact", methods=["POST"])
-def handleAddContact():
+def handleAddContact(): #commit test
 
     name = request.form.get("contact-name")
     number = request.form.get("contact-number")
     contacts.append([name,number])
 
-    return render_template("/show_contacts.html", x = name, y = number)
+    return render_template("/show_contacts.html", name = name, number = number)
 
 if __name__ == "__main__":
     app.run(debug=True, port=80, host="0.0.0.0")
